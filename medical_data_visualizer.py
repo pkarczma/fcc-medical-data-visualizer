be194d4462872d27c2221efdde5cb77020608ff9
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-20 17:55:57 +0000

    feat: normalize cholesterol and glucose column

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -10,6 +10,8 @@ df = pd.read_csv('medical_examination.csv')
 df['overweight'] = (df['weight'] / ((0.01 * df['height']) ** 2) > 25).astype(int)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 
 # Draw Categorical Plot
